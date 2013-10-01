ab380a165859dd41e5bbb587a25bac389f98e750
Dimitrios Zorbas <zorbash@skroutz.gr>
2013-10-01 23:08:14 +0300

    Improve style of ruby/sieve_atkin

--- ruby/sieve_atkin.rb
+++ ruby/sieve_atkin.rb
@@ -5,13 +5,13 @@ nsqrt = Math::sqrt(N).floor
 
 (1..nsqrt).each do |x|
   (1..nsqrt).each do |y|
-    n = 4 * (x * x) + y * y
+    n = 4 * (x ** 2) + y ** 2
     is_prime[n] = !is_prime[n] if n <= N && (n % 12 == 1 || n % 12 == 5)
 
-    n = 3 * (x * x) + y * y
+    n = 3 * (x ** 2) + y ** 2
     is_prime[n] = !is_prime[n] if n <= N && n % 12 == 7
 
-    n = 3 * (x * x) - y * y
+    n = 3 * (x ** 2) - y ** 2
     is_prime[n] = !is_prime[n] if x > y && n <= N && n % 12 == 11
   end
 end
@@ -19,7 +19,7 @@ end
 (5..nsqrt).each do |x|
   next if !is_prime[x]
 
-  y = x * x
+  y = x ** 2
   (y..N-1).step(y).each { |i| is_prime[i] = false }
 end
 
